b04c4231f23218a11fc964a6d1003e6ad94ea577
Santosh Kumar <santosh822kr@gmail.com>
2022-05-11 02:13:42 +0530

    new commit

--- client/src/components/message/Message.jsx
+++ client/src/components/message/Message.jsx
@@ -1,13 +1,34 @@
 import './message.css';
 import { format } from 'timeago.js';
+import { useContext, useEffect, useState } from 'react';
+import { AuthContext } from '../../context/AuthContext';
+import { axiosInstance } from '../../config';
 
 export default function Message({ message, own }) {
+  const [sender, setSender] = useState(null);
+
+  const { user } = useContext(AuthContext);
+  const PF = process.env.REACT_APP_PUBLIC_FOLDER;
+
+  useEffect(() => {
+    const getSender = async () => {
+      try {
+        const res = await axiosInstance.get('/users?userId=' + message.sender);
+        setSender(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    getSender();
+  }, [message, own]);
+
   return (
     <div className={own ? 'message own' : 'message'}>
       <div className='messageTop'>
         <img
           className='messageImg'
-          src='https://images.pexels.com/photos/3686769/pexels-photo-3686769.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500'
+          src={own ? PF + user.profilePicture : PF + sender?.profilePicture}
           alt=''
         />
         <p className='messageText'>{message.text}</p>
